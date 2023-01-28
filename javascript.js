1108a3d829002eafde1381184ae92e9549716450
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-27 20:15:37 -0500

    Add a function for creating the divs

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-for (i=0; i<256; i++){
-
-const square = document.createElement('div');
-square.classList.add('square');
-document.getElementById('container').appendChild(square);
-
+function createDivs(numDivs){
+    for (i=0; i<numDivs; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        document.getElementById('container').appendChild(square);
+    }
 }
+
+let rowCol = 16;
+createDivs(rowCol * rowCol);
