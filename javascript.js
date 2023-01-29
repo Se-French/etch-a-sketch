97919b92f24c736bfafac277754feaf1865a50e6
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-29 14:45:20 -0500

    Create option for grid size
    With the option to choose the grid size the etch-a-sketch will now
    create a grid with the specified number of squares and also still
    function with the color change on hover.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//function to create the specified number of divs
 function createDivs(numDivs){
     for (i=0; i<numDivs; i++){
         const square = document.createElement('div');
@@ -5,17 +6,37 @@ function createDivs(numDivs){
         document.getElementById('container').appendChild(square);
     }
 }
+//button to get number of divs
+const button = document.querySelector('button#howMany');
+button.addEventListener('click', getGrid);
 
-let rowCol = 16;
-createDivs(rowCol * rowCol);
+//function to create etch-a-sketch with specified number of divs
+function getGrid(event){
+    let rowCol;
+    let number = prompt('How many squares per side would you like for the new grid?');
 
+    if (number > 0 && number <= 100){
+        rowCol = number;
+        let numDivs = rowCol * rowCol;
+        createDivs(numDivs);
+    } else if (number > 100){
+        alert('Woah, take it easy! Please choose a number less than 100!');
+    } else if (number <= 0){
+        alert('You can\'t have a grid with no squares!');    
+    } else if (number == undefined || number == null){
+        alert('Please type a number!')
+    }
 
-//change the color of divs on hover
-const allDivs = document.querySelectorAll('div.square');
-console.log(allDivs);
-allDivs.forEach(div => div.addEventListener('mouseover', onHover));
+    //add style to make divs square
+    document.getElementById('container').style.gridTemplateColumns = `repeat(${rowCol}, 1fr)`;
+    document.getElementById('container').style.gridTemplateRows = `repeat(${rowCol}, 1fr)`;
 
-function onHover(event){
-    event.target.style.backgroundColor = 'black';
+    //change the color of divs on hover
+    const allDivs = document.querySelectorAll('div.square');
+    allDivs.forEach(div => div.addEventListener('mouseover', onHover));
+
+    function onHover(event){
+        event.target.style.backgroundColor = 'black';
+    }
+
 }
-
